260952860a008c3df0a586472d8c9fa7046877b9
Glatigny <f.west@live.fr>
2019-01-28 16:10:19 +0100

    exo 3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function greenclick(){
+  document.getElementById("text").style.color="green";
+}
+function redclick(){
+  document.getElementById("text").style.color="red";
+}
+function blueclick(){
+  document.getElementById("text").style.color="blue";
+}
